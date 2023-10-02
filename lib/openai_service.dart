f849bdfff056568f48632142585ba8711fd988c8
VIPIN SINGH MEHRA <vipinsinghmehra535@gmail.com>
2023-10-03 02:19:10 +0530

    api removed

--- lib/openai_service.dart
+++ lib/openai_service.dart
@@ -1,11 +1,27 @@
+import 'dart:convert';
+
+import 'package:bubble/secerts.dart';
 import 'package:http/http.dart' as http;
 
 class OpenAIService {
   Future<String> isArtPromptAPI(String prompt) async {
     try {
       final respone = await http.post(
-        Uri.parse('https://api.openai.com/v1/chat/completions'),
-      );
+          Uri.parse('https://api.openai.com/v1/chat/completions'),
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $openAiAPIKey',
+          },
+          body: jsonEncode({
+            "model": "gpt-3.5-turbo",
+            "messages": [
+            
+              {
+                "role": "user",
+                "content": "Hello!",
+              }
+            ]
+          }));
     } catch (e) {
       return e.toString();
     }
--- lib/secerts.dart
+++ lib/secerts.dart
@@ -1,1 +1,1 @@
-const openAiAPIKey="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";
+const openAiAPIKey = "";
